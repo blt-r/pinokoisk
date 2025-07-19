6e1099b724144f42039bd0c4f08dfea3ce60c139
blt__ <www.edav@gmail.com>
2025-07-19 05:22:26 +0400

    Change MovieCard to not have a button inside of a link

--- src/components/MovieCard.tsx
+++ src/components/MovieCard.tsx
@@ -16,108 +16,107 @@ type Props = {
 const MovieCard: React.FC<Props> = ({ movie }) => {
   return (
     <Paper
-      component={Link}
-      to={'/movie/' + movie.id}
       sx={{
-        textDecoration: 'none',
-        display: 'flex',
-        height: { xs: 180, sm: 250 },
         overflow: 'hidden',
+        position: 'relative',
       }}
     >
       <Box
-        component="img"
-        src={'https://image.tmdb.org/t/p/w500' + movie.poster_path}
-        alt={movie.title + ' poster'}
-        sx={{ height: '100%', aspectRatio: '500 / 720' }}
-      />
-      <Box
+        component={Link}
+        to={'/movie/' + movie.id}
         sx={{
-          fontSize: { xs: '0.7rem', sm: '1rem' },
-          padding: { xs: 1.5, sm: 2 },
-          flexGrow: 1,
-          position: 'relative',
+          textDecoration: 'none',
+          height: { xs: 180, sm: 250 },
+          color: 'inherit',
           display: 'flex',
-          flexDirection: 'column',
-          gap: { xs: 0.5, sm: 1 },
         }}
       >
-        <Box>
-          <Typography
-            variant="h2"
-            sx={{ fontSize: '1.7em', display: 'inline' }}
-          >
-            {movie.title}
-          </Typography>
-          {movie.original_title === movie.title ? null : (
-            <Typography
-              variant="subtitle1"
-              sx={{ display: 'inline', paddingLeft: 1, fontSize: '1.2em' }}
-            >
-              ({movie.original_title})
-            </Typography>
-          )}
-        </Box>
-
+        <Box
+          component="img"
+          src={'https://image.tmdb.org/t/p/w500' + movie.poster_path}
+          alt={movie.title + ' poster'}
+          sx={{ height: '100%', aspectRatio: '500 / 720' }}
+        />
         <Box
           sx={{
+            fontSize: { xs: '0.7rem', sm: '1rem' },
+            padding: { xs: 1.5, sm: 2 },
+            flexGrow: 1,
             display: 'flex',
-            gap: 1.2,
-            alignItems: 'center',
+            flexDirection: 'column',
+            gap: { xs: 0.5, sm: 1 },
           }}
         >
-          <Typography sx={{ fontSize: '1.1em' }}>
-            {movie.release_date.split('-')[0]}
+          <Box>
+            <Typography
+              variant="h2"
+              sx={{ fontSize: '1.7em', display: 'inline' }}
+            >
+              {movie.title}
+            </Typography>
+            {movie.original_title === movie.title ? null : (
+              <Typography
+                variant="subtitle1"
+                sx={{ display: 'inline', paddingLeft: 1, fontSize: '1.2em' }}
+              >
+                ({movie.original_title})
+              </Typography>
+            )}
+          </Box>
+
+          <Box
+            sx={{
+              display: 'flex',
+              gap: 1.2,
+              alignItems: 'center',
+            }}
+          >
+            <Typography sx={{ fontSize: '1.1em' }}>
+              {movie.release_date.split('-')[0]}
+            </Typography>
+
+            <Box sx={{ display: 'flex', alignItems: 'center' }}>
+              <Star sx={{ fontSize: '1em' }} />
+
+              <Typography sx={{ fontSize: '1.1em', ml: 0.2 }}>
+                {movie.vote_average.toFixed(1)}
+              </Typography>
+
+              <Typography sx={{ fontSize: '1.1em', ml: 1.2 }}>
+                ({movie.vote_count.toLocaleString()})
+              </Typography>
+            </Box>
+          </Box>
+
+          <Typography
+            sx={{
+              display: { xs: 'none', sm: ['block', '-webkit-box'] },
+              overflow: 'hidden',
+              textOverflow: 'ellipsis',
+              WebkitLineClamp: 3,
+              WebkitBoxOrient: 'vertical',
+            }}
+          >
+            {movie.overview}
           </Typography>
 
-          <Box sx={{ display: 'flex', alignItems: 'center' }}>
-            <Star sx={{ fontSize: '1em' }} />
-
-            <Typography sx={{ fontSize: '1.1em', ml: 0.2 }}>
-              {movie.vote_average.toFixed(1)}
-            </Typography>
-
-            <Typography sx={{ fontSize: '1.1em', ml: 1.2 }}>
-              ({movie.vote_count.toLocaleString()})
-            </Typography>
-          </Box>
+          <Stack
+            sx={{
+              flexDirection: 'row',
+              flexWrap: 'wrap',
+              gap: 0.5,
+              mt: { sm: 'auto' },
+              pr: { sm: 4 },
+            }}
+          >
+            {movie.genres.map((genre, i) => (
+              <Chip key={i} label={genre.name} size="small" />
+            ))}
+          </Stack>
         </Box>
-
-        <Typography
-          sx={{
-            display: { xs: 'none', sm: ['block', '-webkit-box'] },
-            overflow: 'hidden',
-            textOverflow: 'ellipsis',
-            WebkitLineClamp: 3,
-            WebkitBoxOrient: 'vertical',
-          }}
-        >
-          {movie.overview}
-        </Typography>
-
-        <Stack
-          sx={{
-            flexDirection: 'row',
-            flexWrap: 'wrap',
-            gap: 0.5,
-            mt: { sm: 'auto' },
-            pr: { sm: 4 },
-          }}
-        >
-          {movie.genres.map((genre, i) => (
-            <Chip key={i} label={genre.name} size="small" />
-          ))}
-        </Stack>
-
-        <Box
-          sx={{ position: 'absolute', bottom: '8px', right: '8px' }}
-          onClick={e => {
-            e.stopPropagation();
-            e.preventDefault();
-          }}
-        >
-          <FavoriteButton id={movie.id} />
-        </Box>
+      </Box>
+      <Box sx={{ position: 'absolute', bottom: '8px', right: '8px' }}>
+        <FavoriteButton id={movie.id} />
       </Box>
     </Paper>
   );
