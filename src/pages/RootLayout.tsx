8fef076fe1d944e37f58180d11cb0b949311b356
blt__ <www.edav@gmail.com>
2025-07-26 04:30:20 +0400

    Port RootLayout to tailwind and shadcn

--- src/pages/RootLayout.tsx
+++ src/pages/RootLayout.tsx
@@ -1,46 +1,39 @@
-import Container from '@mui/material/Container';
-import Tabs from '@mui/material/Tabs';
-import Tab from '@mui/material/Tab';
-import FavoriteBorderOutlined from '@mui/icons-material/FavoriteBorderOutlined';
-import ExploreOutlined from '@mui/icons-material/ExploreOutlined';
-import Box from '@mui/material/Box';
 import { Link, Outlet, useLocation } from 'react-router-dom';
+import { Button } from '@/components/ui/button';
+import { Compass, Heart } from 'lucide-react';
 
 const RootLayout: React.FC = () => {
   const location = useLocation();
 
-  const tab = ['/', '/favorites'].includes(location.pathname)
-    ? location.pathname
-    : false;
-
   return (
     <>
-      <Box sx={{ borderBottom: 1, borderColor: 'divider' }} component="nav">
-        <Container>
-          <Tabs value={tab}>
-            <Tab
-              component={Link}
-              to="/"
-              label="Movies"
-              value="/"
-              icon={<ExploreOutlined />}
-              iconPosition="start"
-            />
-            <Tab
-              component={Link}
-              to="/favorites"
-              label="Favorites"
-              value="/favorites"
-              icon={<FavoriteBorderOutlined />}
-              iconPosition="start"
-            />
-          </Tabs>
-        </Container>
-      </Box>
+      <div className="shadow">
+        <nav className="max-w-[1200px] mx-auto w-full px-6 py-4 flex gap-4">
+          <Button
+            asChild
+            size="lg"
+            variant={location.pathname === '/' ? 'secondary' : 'ghost'}
+          >
+            <Link to="/">
+              <Compass />
+              Movies
+            </Link>
+          </Button>
+          <Button
+            asChild
+            size="lg"
+            variant={location.pathname === '/favorites' ? 'secondary' : 'ghost'}
+          >
+            <Link to="/favorites">
+              <Heart /> Favorites
+            </Link>
+          </Button>
+        </nav>
+      </div>
 
-      <Container>
+      <main className="max-w-[1200px] mx-auto w-full px-6">
         <Outlet />
-      </Container>
+      </main>
     </>
   );
 };
