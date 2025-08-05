e1f08c28a3a84db8adbefaae54a59f11c6e71ca3
blt_r <www.edav@gmail.com>
2025-08-05 07:47:53 +0400

    Use FileIcon in MoviePoster

--- src/components/MoviePoster.tsx
+++ src/components/MoviePoster.tsx
@@ -1,3 +1,6 @@
+import { cn } from '@/lib/utils';
+import { FileIcon } from 'lucide-react';
+
 type Props = {
   poster_path: string | null;
   title: string;
@@ -9,39 +12,24 @@ const MoviePoster: React.FC<Props> = ({
   poster_path,
   title,
   sizes,
-  ...props
+  className,
 }) => {
   return poster_path ? (
     <img
-      srcSet={`https://image.tmdb.org/t/p/w92${poster_path} 92w,
-               https://image.tmdb.org/t/p/w154${poster_path} 154w,
-               https://image.tmdb.org/t/p/w185${poster_path} 185w,
-               https://image.tmdb.org/t/p/w300${poster_path} 300w,
-               https://image.tmdb.org/t/p/w500${poster_path} 500w,
-               https://image.tmdb.org/t/p/w780${poster_path} 780w`}
+      srcSet={[92, 154, 185, 300, 500, 780]
+        .map(w => `https://image.tmdb.org/t/p/w${w}${poster_path} ${w}w`)
+        .join(', ')}
       src={`https://image.tmdb.org/t/p/w185${poster_path}`}
       alt={`${title} poster`}
       sizes={sizes}
-      {...props}
+      className={cn('aspect-[2/3]', className)}
     />
   ) : (
-    <div {...props}>
-      <svg
-        width="100%"
-        height="100%"
-        viewBox="0 0 100 100"
-        xmlns="http://www.w3.org/2000/svg"
-        xmlnsXlink="http://www.w3.org/1999/xlink"
-      >
-        <text x="50" y="50" fontSize="12px" fill="black" opacity="0.7">
-          <tspan textAnchor="middle" x="50" dy="-0.9em">
-            Poster
-          </tspan>
-          <tspan textAnchor="middle" x="50" dy="1.2em">
-            not available
-          </tspan>
-        </text>
-      </svg>
+    <div className={cn('aspect-[2/3] grid place-items-center', className)}>
+      <FileIcon
+        className="size-9 stroke-muted-foreground opacity-50"
+        aria-label="poster missing"
+      />
     </div>
   );
 };
